d7c4d7921e2ea102d406498b01259487568fa53f
Marcelo Bonow <marcelobonowott@hotmail.com>
2019-07-14 22:35:30 -0300

    Smooth zoom

--- GameJAAJ/Assets/Scripts/Controls/ZoomBehaviour.cs
+++ GameJAAJ/Assets/Scripts/Controls/ZoomBehaviour.cs
@@ -7,6 +7,8 @@ public class ZoomBehaviour : MonoBehaviour
     [SerializeField] private Camera playerCamera;
 
 
+    private float fovTransitionTime = 0.3f;
+    private Coroutine zoom;
     private bool isZoom;
     private float oldFOV;
 
@@ -19,14 +21,41 @@ public class ZoomBehaviour : MonoBehaviour
             if(!isZoom)
             {
                 isZoom = true;
-                oldFOV = playerCamera.fieldOfView;
+                if(zoom != null)
+                    StopCoroutine(zoom);
+                zoom = StartCoroutine(Zoom());
             }
-            playerCamera.fieldOfView = oldFOV/2f;
         }
         else if(!Input.GetButton("Zoom"))
         {
+            if(isZoom)
+            {
+                if(zoom != null)
+                    StopCoroutine(zoom);
+                zoom = StartCoroutine(Unzoom());
+            }
             isZoom = false;
-            playerCamera.fieldOfView = oldFOV;
+        }
+    }
+
+    private IEnumerator Zoom()
+    {
+        var startTime = Time.time;
+        while(Time.time < startTime + fovTransitionTime)
+        {
+            playerCamera.fieldOfView = Mathf.Lerp(oldFOV, oldFOV/2f, (Time.time-startTime) / fovTransitionTime);
+            Debug.Log("Aumentando zoom: " + playerCamera.fieldOfView);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    private IEnumerator Unzoom()
+    {
+        var startTime = Time.time;
+        while(Time.time < startTime + fovTransitionTime)
+        {
+            playerCamera.fieldOfView = Mathf.Lerp(playerCamera.fieldOfView, oldFOV, (Time.time-startTime) /fovTransitionTime);
+            yield return new WaitForEndOfFrame();
         }
     }
 }

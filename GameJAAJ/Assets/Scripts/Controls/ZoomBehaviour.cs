8ab373ed4a3401f2b05e739b5e4a24033451253a
Marcelo Bonow <marcelobonowott@hotmail.com>
2019-07-14 22:08:26 -0300

    Zoom system implemented

---
+++ GameJAAJ/Assets/Scripts/Controls/ZoomBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomBehaviour : MonoBehaviour
+{
+    [SerializeField] private Camera playerCamera;
+
+
+    private bool isZoom;
+    private float oldFOV;
+
+    private void Awake() => oldFOV = playerCamera.fieldOfView;
+
+    private void Update()
+    {
+        if(Input.GetButton("Zoom"))
+        {
+            if(!isZoom)
+            {
+                isZoom = true;
+                oldFOV = playerCamera.fieldOfView;
+            }
+            playerCamera.fieldOfView = oldFOV/2f;
+        }
+        else if(!Input.GetButton("Zoom"))
+        {
+            isZoom = false;
+            playerCamera.fieldOfView = oldFOV;
+        }
+    }
+}
